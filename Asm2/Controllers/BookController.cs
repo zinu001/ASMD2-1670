7a9189f2d65eb78831c4e3c8bbcc09994602006c
zinu <GiangNTGCH190083@fpt.edu.vn>
2022-12-20 15:12:44 +0700

    Update bookcontroller, cate controller

--- Asm2/Controllers/BookController.cs
+++ Asm2/Controllers/BookController.cs
@@ -1,5 +1,8 @@
 ﻿using Asm2.Data;
+using Asm2.Models;
+using Asm2.ViewModel;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Asm2.Controllers
 {
@@ -13,9 +16,160 @@ namespace Asm2.Controllers
             _context = context;
             this.webHostEnvironment = webHostEnvironment;
         }
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Books.ToListAsync());
+        }
+
+        // GET: Employees/Details/5
+        public async Task<IActionResult> Detail(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Books
+                .FirstOrDefaultAsync(m => m.BookId == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+
+        // GET: Employees/Create
+        public IActionResult Create()
         {
             return View();
+        }
+
+        // POST: Employees/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(BookViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                string uniFileName = UploadFile(model);
+                Book book = new Book
+                {
+                    Title = model.Title,
+                    Description = model.Description,
+                    Quantity = model.Quantity,
+                    Year = model.Year,
+                    Price = model.Price,
+                    ImageUrl = uniFileName,
+                };
+                _context.Add(book);
+                await _context.SaveChangesAsync();
+            }
+            return View();
+        }
+
+        private string UploadFile(BookViewModel model)
+        {
+            string uniFileName = null;
+            if (model.BookPicture != null)
+            {
+                string uploadFolder = Path.Combine(webHostEnvironment.WebRootPath, "images");
+                uniFileName = Guid.NewGuid().ToString() + "_" + model.BookPicture.FileName;
+                string filePath = Path.Combine(uploadFolder, uniFileName);
+                using var fileStream = new FileStream(filePath, FileMode.Create);
+                model.BookPicture.CopyTo(fileStream);
+            }
+            return uniFileName;
+        }
+
+        // GET: Employees/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Books.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return View(book);
+        }
+
+        // POST: Employees/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(BookViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    string uniFileName = UploadFile(model);
+                    Book book = new Book
+                    {
+                        Title = model.Title,
+                        Description = model.Description,
+                        Quantity = model.Quantity,
+                        Year = model.Year,
+                        Price = model.Price,
+                        ImageUrl = uniFileName,
+                    };
+                    _context.Add(book);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BookExists(model.B))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+        // GET: Employees/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Books
+                .FirstOrDefaultAsync(m => m.BookId == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            return View(book);
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var book = await _context.Books.FindAsync(id);
+            _context.Books.Remove(book);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BookExists(int id)
+        {
+            return _context.Books.Any(e => e.BookId == id);
         }
     }
 }
--- Asm2/Controllers/CategoryController.cs
+++ Asm2/Controllers/CategoryController.cs
@@ -1,12 +1,151 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Asm2.Data;
+using Asm2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Asm2.Controllers
 {
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+        private readonly IWebHostEnvironment webHostEnvironment;
+
+        public CategoryController(ApplicationDbContext context, IWebHostEnvironment webHostEnvironment)
+        {
+            _context = context;
+            this.webHostEnvironment = webHostEnvironment;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Categorys.ToListAsync());
+        }
+
+        // GET: Employees/Details/5
+        public async Task<IActionResult> Detail(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cate = await _context.Categorys
+                .FirstOrDefaultAsync(m => m.CategoryId == id);
+            if (cate == null)
+            {
+                return NotFound();
+            }
+
+            return View(cate);
+        }
+
+        // GET: Employees/Create
+        public IActionResult Create()
         {
             return View();
         }
+
+        // POST: Employees/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Category model)
+        {
+            if (ModelState.IsValid)
+            {
+                Category cate = new Category
+                {
+                    CategoryName = model.CategoryName,
+                    CategoryDescription = model.CategoryDescription,
+                };
+                _context.Add(cate);
+                await _context.SaveChangesAsync();
+            }
+            return View();
+        }
+
+        // GET: Employees/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cate = await _context.Categorys.FindAsync(id);
+            if (cate == null)
+            {
+                return NotFound();
+            }
+            return View(cate);
+        }
+
+        // POST: Employees/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Category model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    Category cate = new Category
+                    {
+                        CategoryName = model.CategoryName,
+                        CategoryDescription = model.CategoryDescription,
+                    };
+                    _context.Add(cate);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CateExists(model.CategoryId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+        // GET: Employees/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cate = await _context.Categorys
+                .FirstOrDefaultAsync(m => m.CategoryId == id);
+            if (cate == null)
+            {
+                return NotFound();
+            }
+
+            return View(cate);
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var cate = await _context.Categorys.FindAsync(id);
+            _context.Categorys.Remove(cate);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CateExists(int id)
+        {
+            return _context.Categorys.Any(e => e.CategoryId == id);
+        }
+
     }
 }
